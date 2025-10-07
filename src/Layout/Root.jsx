305beb5793c7fb9003592aea3f612135b38dc068
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-10-07 15:56:03 +0600

    added about section in home page

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../Components/Navbar';
 import { Outlet } from 'react-router-dom';
 import Home from '../Pages/Home';
+import About from '../Pages/About';
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <About></About>
         </div>
     );
 };
